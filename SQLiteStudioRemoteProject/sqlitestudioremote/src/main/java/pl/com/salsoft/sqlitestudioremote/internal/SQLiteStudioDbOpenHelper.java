064d6d71bec18642544113ff478ff2aba038e132
Aleksandr Brazhkin <sirbralex@gmail.com>
2018-09-25 01:59:05 +0300

    Non-safe hack for ignoring db version

--- SQLiteStudioRemoteProject/sqlitestudioremote/src/main/java/pl/com/salsoft/sqlitestudioremote/internal/SQLiteStudioDbOpenHelper.java
+++ SQLiteStudioRemoteProject/sqlitestudioremote/src/main/java/pl/com/salsoft/sqlitestudioremote/internal/SQLiteStudioDbOpenHelper.java
@@ -8,6 +8,9 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by SalSoft Pawel Salawa on 08.02.15.
  */
 public class SQLiteStudioDbOpenHelper extends SQLiteOpenHelper {
+
+    private int realDbVersion;
+
     public SQLiteStudioDbOpenHelper(Context context, String dbPath) {
         super(context, dbPath, null, 1);
     }
@@ -22,5 +25,16 @@ public class SQLiteStudioDbOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+
+    @Override
+    public void onConfigure(SQLiteDatabase db) {
+        realDbVersion = db.getVersion();
+        db.setVersion(1);
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        db.setVersion(realDbVersion);
     }
 }
